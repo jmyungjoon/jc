34a138db82d0a262b3a4f3b3246e63c93f1900be
jmyungjoon <jmyungjoon@gmail.com>
2020-04-22 00:35:26 +0800

    mail

---
+++ mailtest.php
@@ -0,0 +1,10 @@
+<?php
+$to = 'jmyungjoon@gmail.com';
+$subject = 'This is our test mail.';
+$message = '<h1>Hi, there.</h1><p>Thank you for testing</p>';
+$header = "From : The sender name <jmyungjoon@naver.com>\r\n";
+$header .= "Reply-To : jmyungjoon@naver.com\r\n";
+$header .= "Content-type : text/html\r\n";
+
+mail($to, $subject, $message, $header);
+?>
